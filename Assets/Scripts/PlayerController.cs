81e6fa56d2a6b83dac1f75047e2c78a6ded341c5
Tathamjf <1agatha.m.f@gmail.com>
2024-11-27 15:16:28 -0300

    feat: animações do personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //iniciando rigidbody e animator
+    private Rigidbody2D playerRigidBody;
+    public float playerSpeed = 1f;
+
+
+    private bool isWalking;
+    private Animator playerAnimator;
+
+
+
+    
     void Start()
     {
         // obtem e inicializa as propriedades do Rigidbody2D
